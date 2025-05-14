ea662463746d8b0a67aa21b2b8a914a25be59f38
Fabricio Apaza Morales <josueapaza01@gmail.com>
2025-05-14 12:56:14 -0500

    feat(ProductionHistory): added DbContext of my BC

--- ElixirLinePlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ ElixirLinePlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using ElixirLinePlatform.API.ProductionHistory.Domain.Model.Aggregate;
 using ElixirLinePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +30,49 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
       
    
       
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      //=================================================================================================
+      //======================= Fabricio Apaza Bounded Context (Production History) =====================
+      
+      builder.Entity<ProductionRecord>(entity =>
+      {
+         entity.ToTable("production_records");
+         entity.HasKey(e => e.RecordId);
+         entity.Property(e => e.RecordId).ValueGeneratedOnAdd();
+         entity.Property(e => e.BatchId).IsRequired();
+         entity.Property(e => e.StartDate).IsRequired();
+         entity.Property(e => e.EndDate).IsRequired();
+         entity.Property(e => e.VolumeProduced).IsRequired();
+         entity.Property(e => e.QualityMetrics)
+            .HasColumnType("json")
+            .HasConversion(
+               v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+               v => JsonSerializer.Deserialize<Dictionary<string, float>>(v, (JsonSerializerOptions)null) ?? new Dictionary<string, float>()
+            );
+      });
       //Regals de mapped object relational (ORM)
       builder.UseSnakeCaseNamingConvention();
    }
